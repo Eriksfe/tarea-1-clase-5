b41151fccdf39f8dac38b37c957a50b697742201
Eric <schnider.erik@gmail.com>
2021-08-24 22:20:41 -0300

    agrego ;
    agrego ; al final de la línea 14

--- tarea-case-5.js
+++ tarea-case-5.js
@@ -11,7 +11,7 @@ $botonCalcular.onclick = function() {
       imagen.setAttribute("src", "img/sad_penguin.gif");
       textoImagen.textContent = `Cobrás $ ${salarioMensual} por mes. Cómo hacés para vivir?`;
     } else if (salarioMensual > 100001 && salarioMensual < 499999) {
-        imagen.setAttribute("src", "img/woman_meme.png")
+        imagen.setAttribute("src", "img/woman_meme.png");
         textoImagen.textContent = `Cobrás $ ${salarioMensual} por mes. Nada mal, eh!`;
     } else {
         imagen.setAttribute("src", "img/di_caprio.gif");
